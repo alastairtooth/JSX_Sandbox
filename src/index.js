17e20067d2d505d18517dab1caabf2f3327f4130
Alastair Tooth <alastairtooth@gmail.com>
2021-10-14 00:31:33 +1100

    React props basics

--- src/index.js
+++ src/index.js
@@ -1,40 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const fname = "Alastair";
-const lname = "Tooth";
-const num = Math.floor(Math.random() * 10);
-
-
-// Test Code
-const customStyle = {
-  color: "blue",
-  fontSize: "20px",
-  padding: "10px",
-  border: "1px solid black"
-};
-
-ReactDOM.render(
-  <div>
-    <h1>Hello {fname + " " + lname}</h1>
-    <p style={customStyle}>Your favourite number is {num}</p>
-    <div>
-      <img
-        className="image"
-        src="https://source.unsplash.com/random/300x300"
-        alt="something"
-      />
-      <img
-        className="image"
-        src="https://source.unsplash.com/random/301x301"
-        alt="something"
-      />
-      <img
-        className="image"
-        src="https://source.unsplash.com/random/302x302"
-        alt="something"
-      />
-    </div>
-  </div>,
-  document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
