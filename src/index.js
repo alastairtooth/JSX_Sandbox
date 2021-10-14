8e678c35697ec9ec1bbab76f6d67a39c79a4c34d
Alastair Tooth <alastairtooth@gmail.com>
2021-10-14 13:58:34 +1100

    completed mapping exercises for reusable components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+var numbers = [3, 56, 2, 48, 5];
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//Map -Create a new array by doing something with each item in an array.
+
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
